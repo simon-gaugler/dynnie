07a03680a387ba03449e19d3ec532ce8f6436b64
Simon Gaugler <simon@gaugler.de>
2023-10-28 13:50:35 +0200

    dynnie fixes

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,5 @@
 FROM alpine
-RUN apk update && apk add bash wget curl
+RUN apk --no-cache add bash curl bind-tools
 
 COPY dynnie.sh /dynnie.sh
 COPY dynnie_errors.sh /dynnie_errors.sh
--- dynnie.sh
+++ dynnie.sh
@@ -8,40 +8,32 @@ _MYIP="myip"
 _MYIPV6="myipv6"
 _HOSTNAME="hostname"
 
-
-### PROGRAM
-# Check if default variables are set - if not fill with defaults
-setupDefaultVariables
-# Check if Preconfigured Service is chosen and apply values
-applyKnownServiceConfiguration
-checkAllRequiredVariablesSet
-
-# starts the endless update Loop (checking for new IP, updating on change, sleeping intervall)
-updateLoop
-exit 0
-
-
 ### Functions ###
 
 function updateLoop {
+    oldIP=""
+    oldIPV6=""
+
     while :
     do
         updateIpAdresses
-        if [ $IP -ne $oldIP ] || [ $IPV6 -ne $oldIPV6 ]
+        if [ "$IP" != "$oldIP" ] || [ "$IPV6" != "$oldIPV6" ]
         then
             echo "IP Adress changed, old: $oldIP, new: $IP (old IPv6: $oldIPV6, new IPv6: $IPV6)"
-            oldIP = $IP
-            oldIPV6 = $IPV6
+            oldIP=$IP
+            oldIPV6=$IPV6
             sendDynDnsUpdate
+        else
+            echo "IP unchanged, skipping update"
         fi
         sleepIntervalTime
     done
 }
 
 function sendDynDnsUpdate {
-    URL="https://$USERNAME:$PASSWORD@$SERVICE_URL?$URL_VAR_HOSTNAME=$HOSTNAME&$URL_VAR_IP_V4=$IP"
+    URL="https://$SERVICE_URL?$URL_VAR_HOSTNAME=$HOSTNAME&$URL_VAR_IP_V4=$IP"
 
-    if [ -n "$UPDATE_IP_V6"] && [ "$UPDATE_IP_V6" -eq "true"]
+    if [ -n "$UPDATE_IP_V6" ] && [ "$UPDATE_IP_V6" = "true" ]
     then
         URL="$URL&$URL_VAR_IP_V6=$IPV6"
     fi
@@ -52,7 +44,7 @@ function sendDynDnsUpdate {
     fi
 
     echo "Updating DynDns now - calling URL: $URL"
-    RESULT=$(wget -q -O- "$URL")
+    RESULT=$(curl -s -u $USERNAME:$PASSWORD "$URL")
     echo "DynDNS called, result: $RESULT"
 }
 
@@ -66,37 +58,37 @@ function sleepIntervalTime {
 }
 
 function setupDefaultVariables {
-    if [ -z "$URL_VAR_IP_V4"]
+    if [ -z "$URL_VAR_IP_V4" ]
     then
-        URL_VAR_IP_V4="myip"
+        URL_VAR_IP_V4=$_MYIP
     fi
 
-    if [ -z "$URL_VAR_IP_V6"]
+    if [ -z "$URL_VAR_IP_V6" ]
     then
-        URL_VAR_IP_V6="myipv6"
+        URL_VAR_IP_V6=$_MYIPV6
     fi
 
-    if [ -z "$URL_VAR_HOSTNAME"]
+    if [ -z "$URL_VAR_HOSTNAME" ]
     then
-        URL_VAR_HOSTNAME="hostname"
+        URL_VAR_HOSTNAME=$_HOSTNAME
     fi
 
-    if [ -z "$INTERVAL_SEC"]
+    if [ -z "$INTERVAL_SEC" ]
     then
         INTERVAL_SEC=60
     fi
 
-    if [ -z "$DETECT_IP_V4"]
+    if [ -z "$DETECT_IP_V4" ]
     then
         DETECT_IP_V4="true"
     fi
 
-    if [ -z "$DETECT_IP_V6"]
+    if [ -z "$DETECT_IP_V6" ]
     then
         DETECT_IP_V6="false"
     fi
 
-    if [ -z "$UPDATE_IP_V6"]
+    if [ -z "$UPDATE_IP_V6" ]
     then
         UPDATE_IP_V6="false"
     fi
@@ -176,13 +168,13 @@ function checkAllRequiredVariablesSet {
         exit $DYNNIE_ERR_INTERVAL_SEC_REQUIRED
     fi
 
-    if [ -z "$DETECT_IP_V4" ] || [ -z "$SET_IP_V4" ]
+    if [ -z "$DETECT_IP_V4" ] && [ -z "$SET_IP_V4" ]
     then
         echo "Either automatic detection of ipv4 or manual ipv4 must be configured"
         exit $DYNNIE_ERR_DETECT_IP_CONFIG_REQUIRED
     fi
 
-    if [ -z "$DETECT_IP_V6" ] || [ -z "$SET_IP_V6" ]
+    if [ -z "$DETECT_IP_V6" ] && [ -z "$SET_IP_V6" ]
     then
         echo "Either automatic detection of ipv6 or manual ipv6 must be configured"
         exit $DYNNIE_ERR_DETECT_IPV6_CONFIG_REQUIRED
@@ -191,20 +183,21 @@ function checkAllRequiredVariablesSet {
 }
 
 function detectIpv4 {
-    return $(curl -s https://api.ipify.org?format=text)
-    # return $(wget -qO- "http://myexternalip.com/raw")
+    echo "Calling Ipify-API for external IPv4"
+    #IP=$(curl -s -4 --connect-timeout 5 "https://api.ipify.org?format=text")
+    
 }
 
 function detectIpv6 {
-    return $(curl -s https://api64.ipify.org?format=text)
-    # return $(wget -q --output-document - http://checkipv6.dyndns.com/ | grep -o "[0-9a-f\:]\{8,\}")
+    echo "Calling Ipify-API for external IPv6"
+    #IPV6=$(curl -s -6 "https://api6.ipify.org?format=text")
 }
 
 function updateIpAdresses {
-    if [ "$DETECT_IP_V4" -eq "true"] || [ "$DETECT_IP_V4" -eq "1"]
+    if [ "$DETECT_IP_V4" = "true" ] || [ "$DETECT_IP_V4" = "1" ]
     then
         # Detect IPv4 is activated by default
-        IP=$(detectIpv4)
+        IP=$(dig +short -4 myip.opendns.com @resolver1.opendns.com)
         echo "Fetched IPv4 Adress: $IP"
     else
         if [ -z "$SET_IP_V4" ]
@@ -215,12 +208,13 @@ function updateIpAdresses {
         IP=$SET_IP_V4
     fi
 
-    if [ -n "$UPDATE_IP_V6" ] && [ "$UPDATE_IP_V6" -eq "true" ]
+    if [ -n "$UPDATE_IP_V6" ] && [ "$UPDATE_IP_V6" = "true" ]
     then
-        if [ "$DETECT_IP_V6" -eq "true" ] || [ "$DETECT_IP_V6" -eq "1" ]
+        if [ "$DETECT_IP_V6" = "true" ] || [ "$DETECT_IP_V6" = "1" ]
         then
-            # Detect IPv6 is deactivated by default
-            IPV6=$(detectIpv6)
+            # Detect IPv6 is deactivated by default    
+            IPV6=$(dig +short -t aaaa myip.opendns.com @resolver1.opendns.com)
+            #IPV6=$(curl -s -6 https://api64.ipify.org?format=text)
             echo "Fetched IPv6 Adress: $IPV6"
         else
             if [ -z "$SET_IP_V6" ]
@@ -232,3 +226,15 @@ function updateIpAdresses {
         fi
     fi
 }
+
+### PROGRAM
+# Check if default variables are set - if not fill with defaults
+setupDefaultVariables
+# Check if Preconfigured Service is chosen and apply values
+applyKnownServiceConfiguration
+checkAllRequiredVariablesSet
+
+# starts the endless update Loop (checking for new IP, updating on change, sleeping intervall)
+
+updateLoop
+exit 0
